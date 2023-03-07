c4cf7abc4bc7d5ba290b92b8795e40f066b2837c
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-06 18:12:01 -0600

    c

--- myApp.js
+++ myApp.js
@@ -1,6 +1,58 @@
+require('dotenv').config()
+let bodyParser=require('body-parser')
+
 let express = require('express');
 let app = express();
+let count = 0
 
+console.log("Hello World")
+
+app.use("/public",express.static(__dirname +"/public"))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
+
+app.post('/name',(req,res)=>{
+    res.json({
+        name:req.body.first+" "+req.body.last
+    })
+})
+
+
+app.get('/now',(req,res,next)=>{
+    req.time=new Date().toString();
+    console.log(req.time)
+    next();
+},(req,res)=>{
+    res.json({time: req.time})
+})
+
+app.get('/:word/echo',(req,res)=>{
+    res.json({
+        echo: req.params.word
+    })
+})
+
+app.get('/name',(req,res)=>{
+    res.json({ name: `${req.query.first} ${req.query.last}`})
+})
+
+app.get("/",(req,res)=>{
+
+    res.sendFile(__dirname+"/views/index.html")
+})
+
+app.get("/json",(req,res)=>{
+    if(process.env.MESSAGE_STYLE=="uppercase"){
+        res.json({"message": "HELLO JSON"})    
+    }else{
+        res.json({"message": "Hello json"})
+    }
+    
+
+})
 
 
 
